c97844b10e0a34c5e5aedc0c018ece19308aa433
monialisa91 <monikarichter18@gmail.com>
2017-08-29 20:06:55 +0200

    Function calculating the area of a triangle

---
+++ function.js
@@ -0,0 +1,15 @@
+
+function getTriangleArea (a, h) {
+	while( a <= 0 || h <= 0 ) {
+		console.log(" Incorrect data. The base and height must be bigger than 0. Enter the new data. ")
+	}
+	
+	var result = a * h / 2;
+	return result;
+}
+
+console.log (getTriangleArea(10, 6));
+
+var triangle1Area = getTriangleArea (5, 2);
+var triangle2Area = getTriangleArea (7, 1);
+var triangle3Area = getTriangleArea (32, 4);
