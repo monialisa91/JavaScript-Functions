f272e89bcd319afa448864ae6889f4602871ffe8
monialisa91 <monikarichter18@gmail.com>
2017-08-30 12:52:23 +0200

    changes to the file function.js

--- js/function.js
+++ js/function.js
@@ -1,16 +1,17 @@
 
-function getTriangleArea (a, h) {
-	while( a <= 0 || h <= 0 ) {
-		console.log(" Incorrect data. The base and height must be bigger than 0. Enter the new data. ")
+function getTriangleArea(a, h) {
+	if(a <= 0 || h <= 0) {
+		return "Incorrect data. The base and height must be bigger than 0. Enter the new data."
 	}
-	
-	result = a * h / 2;
-	return result;
-
+	else {
+		return a * h / 2;
+	}
 }
 
-console.log (getTriangleArea(10, 6));
+console.log(getTriangleArea(10, 6));
 
-var triangle1Area = getTriangleArea (5, 2);
-var triangle2Area = getTriangleArea (7, 1);
-var triangle3Area = getTriangleArea (32, 4);
+var triangle1Area = getTriangleArea(5, 2);
+var triangle2Area = getTriangleArea(7, 1);
+var triangle3Area = getTriangleArea(-5, 4);
+
+console.log(triangle3Area);
